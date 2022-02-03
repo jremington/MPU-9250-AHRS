ff5ffbd84c57d80baf47beb480dd34b37c64db92
S. James Remington <jremington@users.noreply.github.com>
2022-02-03 09:32:24 -0800

    Angle conversion warning added

--- Mahony2/Mahony2.ino
+++ Mahony2/Mahony2.ino
@@ -124,6 +124,10 @@ void loop()
   // pitch, and then roll.
   // http://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles
   // which has additional links.
+  
+  // Warning: strictly valid only for approximately level movement. This code WILL MALFUNCTION
+  // for certain orientations! DEMONSTRATION VERSION only.
+  
   roll  = atan2((q[0] * q[1] + q[2] * q[3]), 0.5 - (q[1] * q[1] + q[2] * q[2]));
   pitch = asin(2.0 * (q[0] * q[2] - q[1] * q[3]));
   yaw   = atan2((q[1] * q[2] + q[0] * q[3]), 0.5 - ( q[2] * q[2] + q[3] * q[3]));
